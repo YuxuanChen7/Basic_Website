09f0e13f1988fc453e63b3c7dfe8e86ba9d7922b
Yuxuan Chen <yuxuanchen7@gmail.com>
2023-09-19 13:30:22 -0400

    changing error

--- DOM.js
+++ DOM.js
@@ -2,10 +2,10 @@ const btn = document.querySelector("button");
 const textp = document.body.querySelector("#ptext")
 
 function random(number) {
-    return math.floor(math.random()*(number + 1));
+    return Math.floor(Math.random()*(number + 1));
 }
 
 btn.addEventListener("click", () => {
     const rndcolor = `rgb(${random(255)},${random(255)},${random(255)})`;
-    document.body.style.color = rndcolor;
+    textp.style.color = rndcolor;
 });
